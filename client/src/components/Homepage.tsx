c0f58f3a2d2d49c95e553da897dd1a9680995f6e
JAYCEE <Jayceexxiii@gmail.com>
2022-08-10 18:24:23 -0700

    adding admin

--- client/src/components/Homepage.tsx
+++ client/src/components/Homepage.tsx
@@ -24,6 +24,8 @@ const Homepage = () => (
      <Menu.Item key="1"><Link to="/" >Home</Link></Menu.Item>
   
      <Menu.Item key="2"><Link to="/new">New</Link></Menu.Item>
+
+     <Menu.Item key="3"><Link to="/admin">Admin</Link></Menu.Item>
    
 
       </Menu>
