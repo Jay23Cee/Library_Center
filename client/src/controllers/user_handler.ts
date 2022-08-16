6bcfe854315aa112a1e9e9a4077fda65e7fcc66b
JAYCEE <Jayceexxiii@gmail.com>
2022-08-15 19:32:59 -0700

    minor user handler update

--- client/src/controllers/user_handler.ts
+++ client/src/controllers/user_handler.ts
@@ -3,14 +3,13 @@ import { UserLogin } from "../models/users";
 
 export async function User_Login(values:UserLogin){
     const headers = {
-      'withCredentials':true,
         'Content-Type': 'text/plain',
         
       };
       const JSON_string = JSON.stringify(values)
       let link = (process.env.REACT_APP_URL as string);
       let url = link +`/login`
-     const res= axios.post(url,JSON_string,headers).then(response=>{
+     const res= axios.post(url,JSON_string,{'withCredentials': true ,headers}).then(response=>{
       console.log("Sucess ========>,")
      }).catch(error=>{
       throw new TypeError("Username or Password Incorrect")
@@ -42,7 +41,7 @@ export async function User_Login(values:UserLogin){
       const JSON_string = JSON.stringify(values)
       let link = (process.env.REACT_APP_URL as string);
       let url = link +`/signup`
-     const res= axios.post(url,JSON_string,{headers}).then(response=>{
+     const res= axios.post(url,JSON_string,{'withCredentials': true ,headers}).then(response=>{
       console.log("Sucess ========>,")
      }).catch(error=>{
       
