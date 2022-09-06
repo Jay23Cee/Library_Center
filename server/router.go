840039df6c2fd356042c85b6020e9bf1ff5aa49c
JAYCEE <Jayceexxiii@gmail.com>
2022-09-06 00:56:52 -0700

    undo

--- server/router.go
+++ server/router.go
@@ -4,12 +4,24 @@ import (
 	"bookapi/api"
 
 	"net/http"
+	"time"
 
+	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 )
 
 func Connect_router() *chi.Mux {
 	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	fileServer := http.FileServer(http.Dir("./build/"))
+	r.Handle("/*", http.StripPrefix("/", fileServer))
 
 	r.NotFound(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
@@ -18,8 +30,12 @@ func Connect_router() *chi.Mux {
 
 	r.Post("/signup", api.Signup)
 	r.Get("/user", api.GetUser)
+	r.Get("/user/{user_id}", api.GetUser)
 	r.Get("/logout", api.Logout)
 	r.Post("/login", api.Login)
+
+	r.Post("/private/login", api.Private_Login)
+	r.Post("/private/signup", api.Private_Signup)
 
 	r.Route("/api", func(r chi.Router) {
 		r.Post("/add", api.Addbooks)
@@ -27,5 +43,6 @@ func Connect_router() *chi.Mux {
 		r.Get("/read", api.GetBooks)
 		r.Post("/delete", api.Deletebook)
 	})
+
 	return r
 }
