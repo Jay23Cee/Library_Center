52c2f0d984bbed7ac17e91afd5174e7d0cfb61fd
JAYCEE <Jayceexxiii@gmail.com>
2023-06-09 23:54:28 -0700

    remove sass

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,18 +4,14 @@ import Template from "./components/Template";
 import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-function App() {
 
-  
+function App() {
   return (
-    
     <Provider store={store}>
       <div>
-     <HashRouter>
-
-          <Template/>
-     </HashRouter>
-     
+        <HashRouter>
+          <Template />
+        </HashRouter>
       </div>
     </Provider>
   );
